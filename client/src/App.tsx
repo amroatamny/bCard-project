17313cd44c9bc7f3083bf7cbb82dceb0a09549ff
amroatamny <amroa38@gmail.com>
2024-04-19 00:22:24 +0300

    v1.0

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Babel from "./sandbox/babel";
+function App() {
+  return (
+    <div className="App">
+      <Babel />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/sandbox/babel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Babel = () => {
+  return (
+    <>
+      <div>in div</div>
+      <p>in p</p>
+    </>
+  );
+};
+export default Babel;
